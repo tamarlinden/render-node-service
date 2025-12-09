96af3c86c58555e7a59d5b65bf94aafc0b35853b
tamarlinden <t0556797304@gmail.com>
2025-12-09 23:48:32 +0200

    Fix2 MODULE_NOT_FOUND error in index.js require statement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');git add index.js
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
